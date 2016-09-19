c0b836bd5ed1021425cfe7c30a08b6cc8706eb7f
Will Ridgers <will@mindfuzz.net>
2016-09-19 12:45:18 +0100

    Add script that maps compiled JSON sources to the public/sources directory.

---
+++ src/json2public.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import shutil
+
+if __name__ == '__main__':
+  script_directory = os.path.dirname(__file__)
+  root_directory = os.path.abspath(os.path.join(script_directory, '..'))
+
+  map_location = os.path.join(root_directory, 'xml', 'map.json')
+  json_location = os.path.join(root_directory, 'xml', 'json')
+  output_location = os.path.join(root_directory, 'public', 'source')
+
+
+  with open(map_location) as map_file:
+    map_data = json.load(map_file)
+
+  for map_key in map_data.keys():
+      map_value = map_data[map_key]
+
+      source_location = os.path.join(json_location, map_key)
+      source_destination = os.path.join(output_location, map_value)
+
+      print('{} -> {}'.format(source_location, source_destination))
+      shutil.rmtree(source_destination, ignore_errors=True)
+      shutil.copytree(source_location, source_destination)
+
+
+
