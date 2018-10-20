f7e0f6e2dc6bc75d119546c2ac03ee4411371900
Karsten Tinnefeld <k.tinnefeld@gmail.com>
2018-10-20 23:00:24 +0200

    update for Creative Cloud 2019 remove tracking (no privacy statement exists which would allow it) explain the fork and why it exists

--- src/findxml.bat
+++ src/findxml.bat
@@ -9,11 +9,11 @@ mkdir %__xmlsource% 2> nul
 
 copy /y "%CommonProgramFiles(x86)%\Adobe\Scripting Dictionaries CC\CommonFiles\*.xml" %__xmlsource%
 if ERRORLEVEL 1 goto copy
-copy /y "%CommonProgramFiles%\Adobe\Scripting Dictionaries CC\illustrator 2018\omv.xml" %__xmlsource%illustrator.xml
+copy /y "%CommonProgramFiles%\Adobe\Scripting Dictionaries CC\illustrator 2019\omv.xml" %__xmlsource%illustrator.xml
 if ERRORLEVEL 1 goto copy
-copy /y "%CommonProgramFiles(x86)%\Adobe\Scripting Dictionaries CC/photoshop\omv.xml" %__xmlsource%photoshop.xml
+copy /y "%CommonProgramFiles(x86)%\Adobe\Scripting Dictionaries CC\photoshop\omv.xml" %__xmlsource%photoshop.xml
 if ERRORLEVEL 1 goto copy
-copy /y "%CommonProgramFiles%\Adobe\Scripting Dictionaries CC\Adobe Bridge CC 2018\omv.xml" %__xmlsource%bridge.xml
+copy /y "%CommonProgramFiles%\Adobe\Scripting Dictionaries CC\Adobe Bridge CC 2019\omv.xml" %__xmlsource%bridge.xml
 if ERRORLEVEL 1 goto copy
 copy /y "%APPDATA%\Adobe\ExtendScript Toolkit\4.0\omv*.xml" %__xmlsource%
 if %ERRORLEVEL% lss 1 goto end
